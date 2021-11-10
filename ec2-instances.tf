127e93a6a9047a894eecbdfeddcd38dea504db9b
kasdal <plesmilan@hotmail.com>
2021-11-10 21:49:26 +0000

    test script, user data, outputs added

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -8,7 +8,7 @@ module "ec2_public" {
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
-  monitoring             = true
+  #monitoring             = true
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
   tags = local.common_tags
@@ -33,10 +33,55 @@ module "ec2_private" {
     module.vpc.private_subnets[1]
   ]  
   instance_count         = var.private_instance_count
-  user_data = ##USERDATA## to add later
+  user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
 
 
 
 
+
+# EC2 Instance Variables
+
+# AWS EC2 Instance Type
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type = string
+  default = "t3.nano"  
+}
+
+# AWS EC2 Instance Key Pair
+variable "instance_keypair" {
+  description = "Key pair that need to be associated with EC2 Instance"
+  type = string
+  default = "web13"
+}
+
+# AWS EC2 Private Instance Count
+variable "private_instance_count" {
+  description = "AWS EC2 Private Instances Count"
+  type = number
+  default = 1  
+}
+
+
+
+
+
+
+
+
+# AWS EC2 Instances Terraform Outputs
+# Public EC2 Instances - Bastion Host
+
+## ec2_bastion_public_instance_ids
+output "ec2_bastion_public_instance_ids" {
+  description = "List of IDs of instances"
+  value       = module.ec2_public.id
+}
+
+## ec2_bastion_public_ip
+output "ec2_bastion_public_ip" {
+  description = "List of public IP addresses assigned to the instances"
+  value       = module.ec2_public.public_ip 
+}
