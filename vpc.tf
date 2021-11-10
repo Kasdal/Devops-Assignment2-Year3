78db9f1413ad09a6fd7f545fd2742a105aa0780a
kasdal <plesmilan@hotmail.com>
2021-11-10 20:47:45 +0000

    VPC variables and outputs for the module

--- vpc.tf
+++ vpc.tf
@@ -39,3 +39,117 @@ module "vpc" {
   }
 }
 
+
+
+# VPC Input Variables
+
+# VPC Name
+variable "vpc_name" {
+  description = "VPC Name"
+  type = string 
+  default = "myvpc"
+}
+
+# VPC CIDR Block
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  type = string 
+  default = "10.0.0.0/16"
+}
+
+# VPC Availability Zones
+variable "vpc_availability_zones" {
+  description = "VPC Availability Zones"
+  type = list(string)
+  default = ["eu-west-1a", "eu-west-1b"]
+}
+
+# VPC Public Subnets
+variable "vpc_public_subnets" {
+  description = "VPC Public Subnets"
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+# VPC Private Subnets
+variable "vpc_private_subnets" {
+  description = "VPC Private Subnets"
+  type = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+# VPC Database Subnets
+variable "vpc_database_subnets" {
+  description = "VPC Database Subnets"
+  type = list(string)
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+# VPC Create Database Subnet Group (True / False)
+variable "vpc_create_database_subnet_group" {
+  description = "VPC Create Database Subnet Group"
+  type = bool
+  default = true 
+}
+
+# VPC Create Database Subnet Route Table (True or False)
+variable "vpc_create_database_subnet_route_table" {
+  description = "VPC Create Database Subnet Route Table"
+  type = bool
+  default = true   
+}
+
+  
+# VPC Enable NAT Gateway (True or False) 
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
+  type = bool
+  default = true  
+}
+
+# VPC Single NAT Gateway (True or False)
+variable "vpc_single_nat_gateway" {
+  description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
+  type = bool
+  default = true
+}
+
+
+
+# VPC Output Values
+
+# VPC ID
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+# VPC CIDR blocks
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+# VPC Private Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+# VPC Public Subnets
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnet
+}
+
+# VPC NAT gateway Public IP
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = module.vpc.nat_public_ips
+}
+
+# VPC AZs
+output "azs" {
+  description = "A list of availability zones spefified as argument to this module"
+  value       = module.vpc.azs
+}
