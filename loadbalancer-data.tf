309557924626c1524e6892c22dc75f81a0ac33ee
kasdal <plesmilan@hotmail.com>
2021-11-11 14:36:36 +0000

    elastic ip and load balancer data aded

---
+++ loadbalancer-data.tf
@@ -0,0 +1,134 @@
+# Terraform AWS Application Load Balancer (ALB)
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "6.0.0" 
+
+  name = "${local.name}-alb"
+  load_balancer_type = "application"
+  vpc_id = module.vpc.vpc_id
+  /*Optionally, you can specify the subnets individually
+    module.vpc.public_subnets[0],
+    module.vpc.public_subnets[1]
+  ]*/
+  subnets = module.vpc.public_subnets
+  security_groups = [module.loadbalancer_sg.security_group_id]
+  # Listeners
+  # HTTP Listener - HTTP Port 80
+    http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]  
+  # Target Groups
+  target_groups = [
+    # App1 Target Group - TG Index = 0
+    {
+      name_prefix          = "app1-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # App1 Target Group - Targets
+      targets = {
+        my_app1_vm1 = {
+          target_id = module.ec2_private.id[0]
+          port      = 80
+        },
+        my_app1_vm2 = {
+          target_id = module.ec2_private.id[1]
+          port      = 80
+        }
+      }
+      tags =local.common_tags # Target Group Tags*/
+    },  
+  ]
+
+ 
+  tags = local.common_tags # ALB Tags
+}
+
+
+
+
+
+
+# Terraform AWS Application Load Balancer (ALB) Outputs
+output "lb_id" {
+  description = "The ID and ARN of the load balancer i created."
+  value       = module.alb.lb_id
+}
+
+output "lb_arn" {
+  description = "The ID and ARN of the load balancer i created."
+  value       = module.alb.lb_arn
+}
+
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = module.alb.lb_dns_name
+}
+
+output "lb_arn_suffix" {
+  description = "ARN suffix of our load balancer - can be used with CloudWatch."
+  value       = module.alb.lb_arn_suffix
+}
+
+output "lb_zone_id" {
+  description = "The zone_id of the load balancer to assist with creating DNS records."
+  value       = module.alb.lb_zone_id
+}
+
+output "http_tcp_listener_arns" {
+  description = "The ARN of the TCP and HTTP load balancer listeners created."
+  value       = module.alb.http_tcp_listener_arns
+}
+
+output "http_tcp_listener_ids" {
+  description = "The IDs of the TCP and HTTP load balancer listeners created."
+  value       = module.alb.http_tcp_listener_ids
+}
+
+output "https_listener_arns" {
+  description = "The ARNs of the HTTPS load balancer listeners created  *I might use later if i figure out certificates :D *."
+  value       = module.alb.https_listener_arns
+}
+
+output "https_listener_ids" {
+  description = "The IDs of the load balancer listeners created."
+  value       = module.alb.https_listener_ids
+}
+
+output "target_group_arns" {
+  description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
+  value       = module.alb.target_group_arns
+}
+
+
+output "target_group_arn_suffixes" {
+  description = "ARN suffixes of our target groups - i need this for cloud watch later."
+  value       = module.alb.target_group_arn_suffixes
+}
+
+output "target_group_names" {
+  description = "Name of the target group. Dunno yet, might need later.."
+  value       = module.alb.target_group_names
+}
+
+output "target_group_attachments" {
+  description = "ARNs of the target group attachment IDs."
+  value       = module.alb.target_group_attachments
+}
