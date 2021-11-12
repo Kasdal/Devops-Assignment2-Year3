41215e5444afb8768d6182ba2ce4db6dc9466692
kasdal <plesmilan@hotmail.com>
2021-11-12 17:45:30 +0000

    added monitoring script with promethous export

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -11,6 +11,7 @@ module "ec2_public" {
   #monitoring             = true
   subnet_id              = module.vpc.public_subnets[0]
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  user_data = file("${path.module}/monitor.sh")
   tags = local.common_tags
 }
 
